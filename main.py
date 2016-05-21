cc07cdb76acacaa907376e0b0ea503f5caeec87a
AlsoG9x <AlsoG9x@gmail.com>
2016-05-21 20:53:14 +0200

    player mouvement

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-import sys, pygame, time
+import pygame,sys
+from pygame.locals import *
+
+
 pygame.init()
 
-size = width, height = 1920, 1080
-speed = [2,2]
-black = 0, 0, 0
+size = width,height= 320,240
+screen =  pygame.display.set_mode(size)
 
-screen = pygame.display.set_mode(size)
+speed = 4
+ypos = 0
+xpos = 0
+bg=0x998877
+
+#char = sprite_sheet(16,'sprites.png',pos=(16,0)) 
+#charRect = char.get_rect()
+
 char = pygame.image.load("char.bmp")
 charRect = char.get_rect()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]:ypos-=speed
+    if key[pygame.K_DOWN]:ypos+=speed
+    if key[pygame.K_RIGHT]:xpos+=speed
+    if key[pygame.K_LEFT]:xpos-=speed
 
-    charRect = charRect.move(speed)
-    if charRect.left < 0 or charRect.right > width:
-        speed[0] = -speed[0]
-    if charRect.top < 0 or charRect.bottom > height:
-        speed[1] = -speed[1]
+    charRect.left=xpos
+    charRect.top=ypos
 
-    #time.sleep(0.00000001)
-    screen.fill(black)
-    screen.blit(char, charRect)
+    print 'xpos:{} ypos:{}'.format(xpos,ypos)
+
+    screen.fill(bg)
+    screen.blit(char,charRect)
     pygame.display.flip()
+    pygame.time.delay(1000/50)
  
