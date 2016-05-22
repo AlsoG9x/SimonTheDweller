3940c90eedab071ded44a828fa307fd5db787d91
AlsoG9x <AlsoG9x@gmail.com>
2016-05-22 20:25:58 +0200

    added world boundaries

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import pygame,sys
+import pygame,sys,os
 from pygame.locals import *
 
+""" CONSTANTS """
+zsh:1: command not found: q
+speed = 4
+ypos = 32
+xpos = 32
+bg=0x998877
 
+os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
 
-size = width,height= 320,240
 screen =  pygame.display.set_mode(size)
-
-speed = 4
-ypos = 0
-xpos = 0
-bg=0x998877
 
 def spritesheet(file,i,j): #i and j are the location from 0 to N, e.g.: simon is i=4 (5th row) and j=2 (3rd vertial column)
 
@@ -20,6 +21,24 @@ def spritesheet(file,i,j): #i and j are the location from 0 to N, e.g.: simon is
         return sprite0,sprite0rect
 
 simon, simonRect = spritesheet('sprites.png',4,2)
+block, blockRect = spritesheet('sprites.png',39,17)
+
+
+
+
+level = [
+"WWWWWWWWWW"
+"W        W"
+"W        W"
+"W        W"
+"W        W"
+"W        W"
+"W        W"
+"WWWWWWWWWW"
+        ]
+
+
+
 
 while 1:
     for event in pygame.event.get():
@@ -29,6 +48,16 @@ while 1:
     if key[pygame.K_DOWN]:ypos+=speed
     if key[pygame.K_RIGHT]:xpos+=speed
     if key[pygame.K_LEFT]:xpos-=speed
+    
+    while (xpos > width-32):
+        xpos -= 1
+    while (xpos < 0):
+        xpos += +1
+    
+    while (ypos > height-32):
+        ypos -= 1
+    while (ypos < 0):
+        ypos += 1
 
     simonRect.left=xpos
     simonRect.top=ypos
@@ -37,6 +66,7 @@ while 1:
 
     screen.fill(bg)
     screen.blit(simon,simonRect)
-    pygame.display.flip()
+    screen.blit(block,blockRect)
+    pygame.display.update()
     pygame.time.delay(1000/50)
  
