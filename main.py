d24c994a09cdf922e3bcf93a930f00bdd5c204d4
AlsoG9x <AlsoG9x@gmail.com>
2016-05-22 21:01:04 +0200

    fix and comment everything

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame,sys,os
 from pygame.locals import *
 
 """ CONSTANTS """
-zsh:1: command not found: q
+size = width,height= 320,240
 speed = 4
 ypos = 32
 xpos = 32
@@ -13,42 +13,32 @@ pygame.init()
 
 screen =  pygame.display.set_mode(size)
 
-def spritesheet(file,i,j): #i and j are the location from 0 to N, e.g.: simon is i=4 (5th row) and j=2 (3rd vertial column)
 
+#Sprite Sheet function
+def spritesheet(file,i,j):
+#i and j are the location from 0 to N, e.g.: simon is i=4 (5th row) and j=2 (3rd vertial column)
         sheet = pygame.image.load(file)
         sprite0 = sheet.subsurface(i*32,j*32,32,32)
         sprite0rect = sprite0.get_rect()
         return sprite0,sprite0rect
 
+#Importing sprites from the Spritesheet
 simon, simonRect = spritesheet('sprites.png',4,2)
 block, blockRect = spritesheet('sprites.png',39,17)
-
-
-
-
-level = [
-"WWWWWWWWWW"
-"W        W"
-"W        W"
-"W        W"
-"W        W"
-"W        W"
-"W        W"
-"WWWWWWWWWW"
-        ]
-
-
 
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    key = pygame.key.get_pressed()
-    if key[pygame.K_UP]:ypos-=speed
-    if key[pygame.K_DOWN]:ypos+=speed
-    if key[pygame.K_RIGHT]:xpos+=speed
-    if key[pygame.K_LEFT]:xpos-=speed
     
+    #Player Mouvement Inputs
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]|key[pygame.K_z]:ypos-=speed
+    if key[pygame.K_DOWN]|key[pygame.K_s]:ypos+=speed
+    if key[pygame.K_RIGHT]|key[pygame.K_d]:xpos+=speed
+    if key[pygame.K_LEFT]|key[pygame.K_q]:xpos-=speed
+    
+    #World Boundaries
     while (xpos > width-32):
         xpos -= 1
     while (xpos < 0):
@@ -58,12 +48,15 @@ while 1:
         ypos -= 1
     while (ypos < 0):
         ypos += 1
-
+    
+    #Set the player postion
     simonRect.left=xpos
     simonRect.top=ypos
-
+    
+    #Debug line
     print 'xpos:{} ypos:{}'.format(xpos,ypos)
-
+    
+    #Display and update everything
     screen.fill(bg)
     screen.blit(simon,simonRect)
     screen.blit(block,blockRect)
